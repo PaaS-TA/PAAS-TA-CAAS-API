345daffee1b4563bc3f132aaf7c4cf32a7ee0ea0
CissC2 <d3g3nyo@gmail.com>
2018-07-30 09:39:03 +0900

    Caas API Frame생성

---
+++ README.md
@@ -0,0 +1,7 @@
+# PAAS-TA-CAAS-API
+
+## Caas API
+Caas API 란? Kubernetes API 를 제공하는 서비스이다.
+
+## 유의사항
+개발 정보 - 현재 개발중
---
+++ src/main/java/org/paasta/caas/api/PaasTaCaasApiApplication.java
@@ -0,0 +1,12 @@
+package org.paasta.caas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaasTaCaasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaasTaCaasApiApplication.class, args);
+	}
+}
