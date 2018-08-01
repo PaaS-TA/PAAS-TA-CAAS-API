f75d936d04eebeef7eb23424e3e9aedea24ba454
rex <rexx4314@gmail.com>
2018-08-01 18:56:38 +0900

    add comments

--- src/main/java/org/paasta/caas/api/temp/TempController.java
+++ src/main/java/org/paasta/caas/api/temp/TempController.java
@@ -3,11 +3,20 @@ package org.paasta.caas.api.temp;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * The type Temp controller.
+ */
 @RestController
 public class TempController {
 
+    /**
+     * Index string.
+     *
+     * @return the string
+     */
     @RequestMapping
     String index() {
-        return "TEMPORARY INDEX :: 임시 페이지";
+        // TODO:: REMOVE
+        return "CaaS API :: TEMPORARY INDEX :: 임시 페이지";
     }
 }
