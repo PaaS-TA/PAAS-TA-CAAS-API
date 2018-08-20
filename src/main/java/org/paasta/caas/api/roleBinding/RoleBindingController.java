1c86b83b0372c25a2d2ce3512f39ad18bb8e85e2
hrjin <hyunlee@bluedigm.com>
2018-08-20 10:46:45 +0900

    사용자에 권한 할당 API(caas-api)

---
+++ src/main/java/org/paasta/caas/api/roleBinding/RoleBindingController.java
@@ -0,0 +1,68 @@
+package org.paasta.caas.api.roleBinding;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * RoleBinding API 를 호출받는 컨트롤러이다.
+ *
+ * @author hrjin
+ * @version 1.0
+ * @since 2018-08-17
+ */
+@RestController
+@RequestMapping(value = "/roleBindings")
+public class RoleBindingController {
+
+    private final RoleBindingService roleBindingService;
+
+    @Autowired
+    public RoleBindingController(RoleBindingService roleBindingService) {
+        this.roleBindingService = roleBindingService;
+    }
+
+    /**
+     * RoleBinding 객체의 리스트를 조회한다. (모든 네임스페이스에서 조회)
+     *
+     * @return
+     */
+    @GetMapping
+    public RoleBindingList getRoleBindingListByAllNamespace(){
+        return roleBindingService.getRoleBindingListByAllNamespace();
+    }
+
+    /**
+     * RoleBindingList 객체의 리스트를 조회한다. (특정 네임스페이스에서 조회)
+     *
+     * @param namespace
+     * @return Map
+     */
+    @GetMapping(value = "/namespaces/{namespace}/roleBindings")
+    public RoleBindingList getRoleBindingList(@PathVariable("namespace") String namespace){
+        return roleBindingService.getRoleBindingList(namespace);
+    }
+
+    /**
+     * RoleBinding 객체를 조회한다. (특정 네임스페이스에서 조회)
+     *
+     * @param namespace
+     * @param roleBindingName
+     * @return
+     */
+    @GetMapping(value = "/namespaces/{namespace}/roleBindings/{roleBindingName}")
+    public RoleBinding getRoleBinding(@PathVariable("namespace") String namespace, @PathVariable("roleBindingName") String roleBindingName){
+        return roleBindingService.getRoleBinding(namespace, roleBindingName);
+    }
+
+    /**
+     * RoleBind 권한을 할당한다. (특정 네임스페이스에서 조회)
+     *
+     * @param namespace
+     * @param roleBinding
+     * @return
+     */
+    @PostMapping(value = "/namespaces/{namespace}/rolebindings")
+    public RoleBinding createRoleBinding(@PathVariable("namespace") String namespace, @RequestBody RoleBinding roleBinding){
+        return roleBindingService.createRoleBinding(namespace, roleBinding);
+    }
+}
---
+++ src/main/java/org/paasta/caas/api/roleBinding/RoleBindingService.java
@@ -0,0 +1,106 @@
+package org.paasta.caas.api.roleBinding;
+
+import com.google.gson.Gson;
+import org.paasta.caas.api.common.Constants;
+import org.paasta.caas.api.common.PropertyService;
+import org.paasta.caas.api.common.RestTemplateService;
+import org.paasta.caas.api.role.RoleList;
+import org.paasta.caas.api.role.RoleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * RoleBinding Service class
+ *
+ * @author hrjin
+ * @version 1.0
+ * @since 2018-08-17
+ */
+@Service
+public class RoleBindingService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RoleService.class);
+    private final RestTemplateService restTemplateService;
+
+    @Autowired
+    public RoleBindingService(RestTemplateService restTemplateService) {
+        this.restTemplateService = restTemplateService;
+    }
+
+    @Autowired
+    PropertyService propertyService;
+
+
+    /**
+     * RoleBindingList 조회 (모든 네임스페이스에서 조회)
+     *
+     * @return Map
+     */
+    public RoleBindingList getRoleBindingListByAllNamespace() {
+        String apiUrl = propertyService.getCaasMasterApiListRoleBindingAllListUrl();
+
+        HashMap hashMap = (HashMap) restTemplateService.send(Constants.TARGET_CAAS_MASTER_API, apiUrl, HttpMethod.GET, null, Map.class);
+        LOGGER.info("########## getRoleBindingListByAllNamespaces() :: hashMap.toString() :: {}", hashMap.toString());
+
+        Gson gson = new Gson();
+        return gson.fromJson(gson.toJson(hashMap), RoleBindingList.class);
+    }
+
+    /**
+     * RoleBindingList 조회 (특정 네임스페이스에서 조회)
+     *
+     * @return Map
+     */
+    public RoleBindingList getRoleBindingList(String namespace) {
+        String apiUrl = propertyService.getCaasMasterApiListRoleBindingListUrl().replaceAll("\\{" + "namespace" + "\\}", namespace);
+
+        HashMap hashMap = (HashMap) restTemplateService.send(Constants.TARGET_CAAS_MASTER_API, apiUrl, HttpMethod.GET, null, Map.class);
+        LOGGER.info("########## getRoleBindingList() :: hashMap.toString() :: {}", hashMap.toString());
+
+        Gson gson = new Gson();
+        return gson.fromJson(gson.toJson(hashMap), RoleBindingList.class);
+    }
+
+    /**
+     * RoleBinding 객체를 조회한다. (특정 네임스페이스에서 조회)
+     *
+     * @param namespace
+     * @param roleBindingName
+     * @return
+     */
+    public RoleBinding getRoleBinding(String namespace, String roleBindingName) {
+        String apiUrl = propertyService.getCaasMasterApiListRoleBindingGetUrl()
+                .replaceAll("\\{" + "namespace" + "\\}", namespace)
+                .replaceAll("\\{" + "name" + "\\}", roleBindingName);
+
+        HashMap hashMap = (HashMap) restTemplateService.send(Constants.TARGET_CAAS_MASTER_API, apiUrl, HttpMethod.GET, null, Map.class);
+        LOGGER.info("########## getRole() :: hashMap.toString() :: {}", hashMap.toString());
+
+        Gson gson = new Gson();
+        return gson.fromJson(gson.toJson(hashMap), RoleBinding.class);
+    }
+
+
+    /**
+     * RoleBind 권한을 할당한다. (특정 네임스페이스에서 조회)
+     *
+     * @param namespace
+     * @param roleBinding
+     * @return
+     */
+    public RoleBinding createRoleBinding(String namespace, RoleBinding roleBinding) {
+        String apiUrl = propertyService.getCaasMasterApiListRoleBindingCreateUrl().replaceAll("\\{" + "namespace" + "\\}", namespace);
+
+        HashMap hashMap = (HashMap) restTemplateService.send(Constants.TARGET_CAAS_MASTER_API, apiUrl, HttpMethod.POST, roleBinding, Map.class);
+        LOGGER.info("########## getRoleBindingList() :: hashMap.toString() :: {}", hashMap.toString());
+
+        Gson gson = new Gson();
+        return gson.fromJson(gson.toJson(hashMap), RoleBinding.class);
+    }
+}
