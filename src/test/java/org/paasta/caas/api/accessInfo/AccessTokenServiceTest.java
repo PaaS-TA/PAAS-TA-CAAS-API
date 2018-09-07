f5554c05a9508f2737ac00d0f9b0a5d665c6ab47
hyerin <hyerin@bluedigm.com>
2018-09-07 14:56:42 +0900

    AccessTokenService 수정 및 테스트케이스 작성

--- src/test/java/org/paasta/caas/api/accessInfo/AccessTokenServiceTest.java
+++ src/test/java/org/paasta/caas/api/accessInfo/AccessTokenServiceTest.java
@@ -21,50 +21,41 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @TestPropertySource("classpath:application.yml")
+
 public class AccessTokenServiceTest {
     private static final String NAMESPACE = "test-namespace";
     private static final String ACCESS_TOKEN_NAME = "test-access-token-name";
-    private static final String GET_URL = "test-get-url";
+    private static final String ENCODE_TOKEN = "YWNjZXNzVG9rZW4=";
+    private static final String DECODE_TOKEN = "accessToken";
 
     private static HashMap gResultMap = null;
     private static AccessToken gResultModel = null;
     private static AccessToken gFinalResultModel = null;
-    private static Map gResultDataMap = null;
-    private static Map gResultDataFinalMap = null;
+    private static HashMap gResultDataMap = null;
 
     @Mock
     RestTemplateService restTemplateService;
-
-    @Mock
-    PropertyService propertyService;
-
-    @Mock
-    CommonService commonService;
 
     @InjectMocks
     AccessTokenService accessTokenService;
 
     @Before
     public void setUp() {
+
         gResultMap = new HashMap();
+        gResultDataMap = new HashMap<>();
+        gResultDataMap.put("token", ENCODE_TOKEN);
+        gResultMap.put("data", gResultDataMap);
+
 
         gResultModel = new AccessToken();
         gFinalResultModel = new AccessToken();
 
-        gResultDataMap = new HashMap<>();
-        gResultDataMap.put("ca.crt", "crt-token=");
-        gResultDataMap.put("namespace", "test-namespace");
-        gResultDataMap.put("token", "accessToken");
-
-
-        gResultDataFinalMap = new HashMap<>();
-        gResultDataFinalMap.put("data", gResultDataMap);
     }
 
     @After
@@ -76,18 +67,14 @@ public class AccessTokenServiceTest {
 
         String token = "accessToken";
         // CONDITION
-        //when(propertyService.getCaasMasterApiListAccessInfoGetUrl()).thenReturn(GET_URL);
-        when(restTemplateService.send(Constants.TARGET_CAAS_MASTER_API, "/api/v1/namespaces" + NAMESPACE + "/secrets/" + ACCESS_TOKEN_NAME, HttpMethod.GET, null, Map.class)).thenReturn(gResultMap);
-        when(gResultMap.get("data")).thenReturn(gResultDataMap);
-        when(gResultDataMap.get("token").toString()).thenReturn(token);
+        when(restTemplateService.send(Constants.TARGET_CAAS_MASTER_API, "/api/v1/namespaces/test-namespace/secrets/test-access-token-name", HttpMethod.GET, null, Map.class)).thenReturn(gResultMap);
 
         // TEST
         AccessToken resultModel = accessTokenService.getSecret(NAMESPACE, ACCESS_TOKEN_NAME);
 
         // VERIFY
         assertThat(resultModel).isNotNull();
-
-        //assertEquals(Constants.RESULT_STATUS_SUCCESS, resultModel.getResultCode());
+        assertEquals(resultModel.getUserAccessToken(), DECODE_TOKEN);
     }
 
 }
