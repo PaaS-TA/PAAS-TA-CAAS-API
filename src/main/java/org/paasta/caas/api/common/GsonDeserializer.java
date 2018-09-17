f4c7a080d98c9c2ae25b91ea848da65c027fb170
rex <rexx4314@gmail.com>
2018-09-17 16:15:37 +0900

    TODO :: remove unused file after check

--- src/main/java/org/paasta/caas/api/common/GsonDeserializer.java
+++ src/main/java/org/paasta/caas/api/common/GsonDeserializer.java
@@ -1,28 +1,29 @@
-package org.paasta.caas.api.common;
-
-import com.google.gson.*;
-import org.slf4j.LoggerFactory;
-
-import java.lang.reflect.Type;
-import java.math.BigDecimal;
-import java.util.logging.Logger;
-
-public class GsonDeserializer implements JsonDeserializer<Integer> ,JsonSerializer<Integer> {
-
-    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(GsonDeserializer.class);
-
-    @Override
-    public JsonElement serialize ( Integer src, Type typeOfSrc, JsonSerializationContext context ) {
-
-        JsonElement element = new JsonPrimitive(src.toString());
-
-        return element;
-    }
-
-    @Override
-    public Integer deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-
-        return json.getAsJsonPrimitive().getAsInt();
-    }
-
-}
+//TODO :: REMOVE AFTER CHECK
+//package org.paasta.caas.api.common;
+//
+//import com.google.gson.*;
+//import org.slf4j.LoggerFactory;
+//
+//import java.lang.reflect.Type;
+//import java.math.BigDecimal;
+//import java.util.logging.Logger;
+//
+//public class GsonDeserializer implements JsonDeserializer<Integer> ,JsonSerializer<Integer> {
+//
+//    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(GsonDeserializer.class);
+//
+//    @Override
+//    public JsonElement serialize ( Integer src, Type typeOfSrc, JsonSerializationContext context ) {
+//
+//        JsonElement element = new JsonPrimitive(src.toString());
+//
+//        return element;
+//    }
+//
+//    @Override
+//    public Integer deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+//
+//        return json.getAsJsonPrimitive().getAsInt();
+//    }
+//
+//}
