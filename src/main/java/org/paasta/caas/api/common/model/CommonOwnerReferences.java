b56b48f64e58a7adb3cf508b506ae3bb9c107ac6
비디 조현구 <hgcho@bluedigm.com>
2018-09-19 12:57:56 +0900

    Refactor classes - Target classes : CommonOwnerReference, SecretVolumeSource, Volume, CommonStatus, CommonCondition, CommonMetaData - Changelogs  -- Move package of OwnerReference and rename to CommonOwnerReference.  -- Rename Secret to SecretVolumeSource  -- Add class description for SecretVolumeSource, Volume.  -- Add missing annotation in CommonStatus  -- Add getter method for timestamp variable; CommonCondition, CommonMetaData.

--- src/main/java/org/paasta/caas/api/common/model/CommonOwnerReferences.java
+++ src/main/java/org/paasta/caas/api/common/model/CommonOwnerReferences.java
@@ -1,19 +1,20 @@
-package org.paasta.caas.api.workloads.pods.support;
+package org.paasta.caas.api.common.model;
 
 import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 /**
- * Common OwnerReferences Model 클래스
+ * Common CommonOwnerReferences Model 클래스
  *
  * @author hyerin
  * @version 1.0
  * @since 2018.09.19
  */
 @Data
-public class OwnerReferences {
-
+class CommonOwnerReferences {
     @SerializedName(value = "name")
     private String name;
 
+    @SerializedName(value = "controller")
+    private boolean controller;
 }
--- src/main/java/org/paasta/caas/api/workloads/pods/support/SecretVolumeSource.java
+++ src/main/java/org/paasta/caas/api/workloads/pods/support/SecretVolumeSource.java
@@ -3,8 +3,15 @@ package org.paasta.caas.api.workloads.pods.support;
 import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
+/**
+ * Secret Volume Source Model 클래스 (Secret 클래스가 아님)
+ *
+ * @author Hyungu Cho
+ * @version 1.0
+ * @since 2018.09.04
+ */
 @Data
-public class Secret {
+class SecretVolumeSource {
 
     @SerializedName(value = "secretName")
     private String secretName;
